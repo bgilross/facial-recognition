27ba9849ebffde68730c083eb0f5bf2e8570d23a
bgilross <bgilross@gmail.com>
2024-08-08 15:51:56 -0700

    Set up rank component, add some styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css'
 import Navigation from './Components/Navigation/Navigation'
 import Logo from './Components/Logo/Logo'
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
+import Rank from './Components/Rank/Rank'
+import ParticlesBg from 'particles-bg'
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg className="particles" type="cobweb" bg={true} />
       Facial Recognition App
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,9 @@
+const Rank = () => {
+  return (
+    <div>
+      <div className="white f3">{'Your Current Rank is...'}</div>
+      <div className="white f1">{'#5'}</div>
+    </div>
+  )
+}
+export default Rank
